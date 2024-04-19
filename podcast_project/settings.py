39eb0bd0fe03a5b18edd7d2403621219a39e6946
MohammadSiamaki <msiamaki.4@gmail.com>
2024-04-19 19:14:50 +0330

    completed the content models and customized the base User model and completed the user_interface app's models

--- podcast_project/settings.py
+++ podcast_project/settings.py
@@ -118,3 +118,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Changing the default User model and using the CustomUser model instead
+
+AUTH_USER_MODEL = "user_interface.CustomUser"
---
+++ user_interface/models.py
@@ -0,0 +1,79 @@
+from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
+
+from base_model.base_model import BaseModel
+from content.models import Episode
+
+# Create your models here.
+
+
+class CustomUser(AbstractUser):
+    profile_picture = models.ImageField(upload_to="profile_pictures/", verbose_name="Profile Pictures")
+    bio = models.TextField(verbose_name="Bio")
+    email = models.EmailField(verbose_name="Email")
+    social_link = models.URLField(verbose_name="Social Link")
+
+    class Meta:
+        db_table = "custom_user"
+        verbose_name = "Custom User"
+        verbose_name_plural = "Custom Users"
+        ordering = ("username",)
+
+
+class Channel(BaseModel):
+    name = models.CharField(max_length=255, unique=True, verbose_name="Channel's Name")
+    logo = models.ImageField(upload_to="logos/", verbose_name="Channel's Logo")
+
+    class Meta:
+        ordering = ("name",)
+        verbose_name = "Channel"
+        verbose_name_plural = "Channels"
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class ChannelOwner(BaseModel):
+    channel = models.ForeignKey(
+        Channel,
+        related_name="channel_owner",
+        verbose_name="Channel",
+        on_delete=models.CASCADE,
+    )
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name="channel_owner",
+        verbose_name="User",
+        on_delete=models.CASCADE,
+    )
+
+    class Meta:
+        verbose_name = "Channel & Owner"
+        verbose_name_plural = "Channels & Owners"
+
+    def __str__(self) -> str:
+        return self.channel.name
+
+
+class ChannelEpisode(BaseModel):
+    channel = models.ForeignKey(
+        Channel,
+        on_delete=models.CASCADE,
+        related_name="channel_episode",
+        verbose_name="Channel",
+    )
+    episode = models.ForeignKey(
+        Episode,
+        on_delete=models.CASCADE,
+        related_name="channel_episode",
+        verbose_name="Episode",
+    )
+
+    class Meta:
+        verbose_name = "Channel's Episode"
+        verbose_name_plural = "Channel's Episodes"
+        ordering = ("created_date",)
+
+    def __str__(self) -> str:
+        return self.channel.name + self.episode.title
