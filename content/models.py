86d40d8b6e3e42c8a77f132b9f435ec0fd6baee4
MohammadSiamaki <msiamaki.4@gmail.com>
2024-04-19 16:04:25 +0330

    Created the content app and implemented the required models

--- content/models.py
+++ content/models.py
@@ -1,18 +1,3 @@
 from django.db import models
 
-from base_model.base_model import BaseModel
-
 # Create your models here.
-
-
-class Episode(BaseModel):
-    title = models.TextField(verbose_name="Episode Title")
-    audio_file = models.FileField(upload_to="audios/", verbose_name="Audio Content")
-
-    class Meta:
-        verbose_name = "Episode"
-        verbose_name_plural = "Episodes"
-        ordering = ['created_date']
-
-    def __str__(self):
-        return self.title
