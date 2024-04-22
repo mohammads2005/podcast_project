5303b11f355f362a499c5460c45ab4650fe243c3
MohammadSiamaki <msiamaki.4@gmail.com>
2024-04-22 14:37:41 +0330

    Created the view for the previously added form in content's app

--- content/views.py
+++ content/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+
+from .forms import EpisodeForm
+# from .models import Episode
 
 # Create your views here.
+
+
+def upload_new_podcast(request):
+    if request.method == 'POST':
+        form = EpisodeForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+
+            return HttpResponse("File created successfully")
+
+    else:
+        form = EpisodeForm()
+
+        return render(request, "content/new_episode_form.html", {'form': form})
