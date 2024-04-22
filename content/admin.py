fd64c84a4480f47d238d6a7bc295030f516052c5
MohammadSiamaki <msiamaki.4@gmail.com>
2024-04-22 13:16:11 +0330

    Created the EpisodeAdmin model in content's admin.py and added the upload form in the content app

--- content/admin.py
+++ content/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.admin import register
+
+from .models import Episode, ChannelEpisode
 
 # Register your models here.
+
+
+@register(Episode)
+class EpisodeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'description', 'audio_file', 'created_date')
+    list_filter = ('created_date', 'updated_date', 'channel_mentions')
+    list_display_links = ('id', 'title')
---
+++ content/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+
+from content.models import Episode
+# from user_interface.models import Channel
+
+
+class EpisodeForm(forms.ModelForm):
+    class Meta:
+        model = Episode
+        fields = (
+            "title", "description", "audio_file", "channel_mentions",
+        )
