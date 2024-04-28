396a9edd6b0f279e020253dc43b6a76fd1f6d3ca
MohammadSiamaki <msiamaki.4@gmail.com>
2024-04-28 13:47:52 +0330

    Implemented the views for episodes and adding a new episode

--- content/views.py
+++ content/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render, HttpResponse
 
 from .forms import EpisodeForm
-# from .models import Episode
+from .models import Episode
+from .serializers import EpisodeSerializer
+
+from rest_framework import viewsets, permissions, status
+from rest_framework.response import Response
+
 
 # Create your views here.
+
+
+class EpisodeModelView(viewsets.ModelViewSet):
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    serializer_class = EpisodeSerializer
+    queryset = Episode.objects.all().order_by('id')
+
+    def create(self, request, *args, **kwargs):
+        return Response({"detail": "POST method is not allowed."}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    # def post(request, *args, **kwargs):
+    #     return Response({"detail": "POST method is not allowed."}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
 
 def upload_new_podcast(request):
