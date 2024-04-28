3bb4541e146a0bff2928cdcd3644ddb42b2fda84
MohammadSiamaki <msiamaki.4@gmail.com>
2024-04-28 14:37:23 +0330

    user_interface's admin panel is complete for already existing models in the same app

--- user_interface/admin.py
+++ user_interface/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from django.contrib.admin import register
+from django.contrib.auth.admin import UserAdmin
+
+from .models import CustomUser, Channel, ChannelOwner
 
 # Register your models here.
+
+
+@register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('id', 'username', 'email', 'first_name', 'last_name')
+    list_display_links = ('id', 'username', 'email', 'first_name', 'last_name')
+    list_filter = ('date_joined',)
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+
+
+@register(Channel)
+class ChannelAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description')
+    list_display_links = ('id', 'name')
+    list_filter = ('created_date', 'updated_date')
+    search_fields = ('name', 'description')
+
+
+@register(ChannelOwner)
+class ChannelOwnerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user_id', 'channel_name')
+    list_display_links = ('id',)
+    list_filter = ('created_date', 'updated_date')
+
+    @admin.display(ordering='id', description='Owner ID')
+    def user_id(self, obj):
+        return obj.user.id
+
+    @admin.display(ordering='id', description='Channel Name')
+    def channel_name(self, obj):
+        return obj.channel.name
