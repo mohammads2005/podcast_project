ddf6aa2c23302047ffeb78dd74cf5d2554040e94
MohammadSiamaki <msiamaki.4@gmail.com>
2024-04-28 13:44:54 +0330

    The newly added EpisodeMentions model is now available in admin panel

--- content/admin.py
+++ content/admin.py
@@ -1,13 +1,57 @@
 from django.contrib import admin
 from django.contrib.admin import register
 
-from .models import Episode, ChannelEpisode
+from .models import Episode, EpisodeChannel, EpisodeMentions
 
 # Register your models here.
 
 
 @register(Episode)
 class EpisodeAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'description', 'audio_file', 'created_date')
-    list_filter = ('created_date', 'updated_date', 'channel_mentions')
+    list_display = (
+        'id',
+        'title',
+        'description',
+        'audio_file',
+        'created_date',
+    )
     list_display_links = ('id', 'title')
+    list_filter = ('created_date', 'updated_date')
+    search_fields = ('title', 'description')
+
+
+@register(EpisodeChannel)
+class EpisodeChannelAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'get_episode_title',
+        'get_channel_name',
+        'created_date',
+    )
+    list_display_links = ('id',)
+    list_filter = ('created_date', 'updated_date')
+
+    @admin.display(ordering='created_date', description='Title')
+    def get_episode_title(self, obj):
+        return obj.episode.title
+
+    @admin.display(ordering='created_date', description='Channel Name')
+    def get_channel_name(self, obj):
+        return obj.channel.name
+
+
+@register(EpisodeMentions)
+class EpisodeMentionsAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'get_episode_title', 'get_channel_name',
+    )
+    list_display_links = ('id',)
+    list_filter = ('created_date', 'updated_date')
+
+    @admin.display(ordering='created_date', description='Title')
+    def get_episode_title(self, obj):
+        return obj.episode.title
+
+    @admin.display(ordering='created_date', description='Channel Name')
+    def get_channel_name(self, obj):
+        return obj.channel.name
