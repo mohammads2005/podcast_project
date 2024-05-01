5fe7a8e2a7bfa0bb2da1ea1320e53e4d2ca376dd
MohammadSiamaki <msiamaki.4@gmail.com>
2024-05-01 19:16:02 +0330

    user_interface's admi panel is now complete

--- user_interface/admin.py
+++ user_interface/admin.py
@@ -9,15 +9,49 @@ from .models import CustomUser, Channel, ChannelOwner
 
 @register(CustomUser)
 class CustomUserAdmin(UserAdmin):
-    list_display = ('id', 'username', 'email', 'first_name', 'last_name')
-    list_display_links = ('id', 'username', 'email', 'first_name', 'last_name')
-    list_filter = ('date_joined',)
-    search_fields = ('username', 'email', 'first_name', 'last_name')
+    list_display = (
+        'id',
+        'username',
+        'picture_tag',
+        'email',
+        'first_name',
+        'last_name',
+    )
+    readonly_fields = (
+        'date_joined',
+        'last_login',
+        'picture_tag',
+    )
+    list_display_links = (
+        'id',
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+    )
+    search_fields = (
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+    )
+    list_filter = ('date_joined', 'last_login')
 
 
 @register(Channel)
 class ChannelAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'description')
+    list_display = (
+        'id',
+        'name',
+        'logo_tag',
+        'description',
+    )
+    readonly_fields = (
+        'created_date',
+        'updated_date',
+        'logo_tag',
+        'description',
+    )
     list_display_links = ('id', 'name')
     list_filter = ('created_date', 'updated_date')
     search_fields = ('name', 'description')
@@ -25,13 +59,13 @@ class ChannelAdmin(admin.ModelAdmin):
 
 @register(ChannelOwner)
 class ChannelOwnerAdmin(admin.ModelAdmin):
-    list_display = ('id', 'user_id', 'channel_name')
+    list_display = ('id', 'user_info', 'channel_name')
     list_display_links = ('id',)
     list_filter = ('created_date', 'updated_date')
 
     @admin.display(ordering='id', description='Owner ID')
-    def user_id(self, obj):
-        return obj.user.id
+    def user_info(self, obj):
+        return f"{obj.user.id}. {obj.user.username}"
 
     @admin.display(ordering='id', description='Channel Name')
     def channel_name(self, obj):
