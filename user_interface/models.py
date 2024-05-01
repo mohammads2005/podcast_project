2ff9aed9ed7970a655362d160b99c50c832d3702
MohammadSiamaki <msiamaki.4@gmail.com>
2024-05-01 19:17:05 +0330

    Some last minute modifications to models of user_interface

--- user_interface/models.py
+++ user_interface/models.py
@@ -1,6 +1,8 @@
+from django.core.exceptions import ObjectDoesNotExist
 from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
+from django.utils.html import mark_safe
 
 from base_model.base_model import BaseModel
 
@@ -9,10 +11,21 @@ from base_model.base_model import BaseModel
 
 class CustomUser(AbstractUser):
     profile_picture = models.ImageField(
-        upload_to="profile_pictures/", verbose_name="Profile Pictures"
+        upload_to="profile_pictures/",
+        verbose_name="Profile Pictures",
+        blank=True,
+        null=True,
     )
-    bio = models.TextField(verbose_name="Bio", blank=True)
+    bio = models.TextField(max_length=255, verbose_name="Bio", blank=True)
     social_link = models.URLField(verbose_name="Social Link", blank=True)
+
+    def picture_tag(self):
+        if self.profile_picture:
+            return mark_safe(
+                '<img src="/media/{}" width=128px/>'.format(self.profile_picture)
+            )
+        else:
+            return "No Picture Found!"
 
     class Meta:
         db_table = "custom_user"
@@ -29,6 +42,14 @@ class Channel(BaseModel):
         upload_to="logos/", verbose_name="Channel's Logo"
     )
 
+    def logo_tag(self):
+        if self.logo:
+            return mark_safe(
+                '<img src="/media/{}" width=128px/>'.format(self.logo)
+            )
+        else:
+            return "No Logo Found!"
+
     class Meta:
         ordering = ("name",)
         verbose_name = "Channel"
@@ -36,6 +57,14 @@ class Channel(BaseModel):
 
     def __str__(self) -> str:
         return self.name
+
+
+class ChannelOwnerManager(models.Manager):
+    def get_or_none(self, **kwargs):
+        try:
+            return self.get(**kwargs)
+        except ObjectDoesNotExist:
+            return None
 
 
 class ChannelOwner(BaseModel):
@@ -51,6 +80,7 @@ class ChannelOwner(BaseModel):
         verbose_name="User",
         on_delete=models.CASCADE,
     )
+    objects = ChannelOwnerManager()
 
     class Meta:
         verbose_name = "Channel & Owner"
